30c3308ce1d47143c1cd0be3313544017827c9b2
Henrique Sach Aguiar <henrique.sachtaguiar@ucalgary.ca>
2026-03-06 12:03:50 -0700

    ex2 (in progress)

--- jsx-exercise/src/App.js
+++ jsx-exercise/src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Constrols from './Controls.js';
+import UserList from './UserList.js';
 
 function App() {
   let currentYear = new Date().getFullYear();
   let isLoggedIn = true;
   return (
-    <div>
-      <h1>ENSF-381: Full Stack Web Development</h1>
-      <p>React Components</p>
-      <p>{currentYear}</p>
-      <p>{isLoggedIn ? "Welcome Back!" : "Please log in."}</p>
-    </div>
+  <div>
+    <h1>ENSF-381: Full Stack Web Development</h1>
+    <p>React Components</p>
+    <p>{currentYear}</p>
+    <p>{isLoggedIn ? "Welcome Back!" : "Please log in."}</p>
+    <Constrols />
+    <UserList />
+  </div>
 
   );
 }
